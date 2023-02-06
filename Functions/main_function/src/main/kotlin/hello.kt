368a5cc3771de1fe4ceed79a3b30459fe216e8f1
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-02-06 00:54:27 -0800

    functions

--- Functions/main_function/src/main/kotlin/hello.kt
+++ Functions/main_function/src/main/kotlin/hello.kt
@@ -1,11 +1,4 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-    temperature()
 }
-fun temperature(){
-    val temperature = 10
-    val isHot = if (temperature > 50) true else false
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
-    println(message)
-    println(isHot)
-}
+
