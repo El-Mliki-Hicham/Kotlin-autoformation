17e1ad0042a2e85ad92c0c1047fb85ef4c064c52
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-02-09 00:52:02 -0800

    exercice

--- Kotlin Basic/exercice/src/main/kotlin/Main.kt
+++ Kotlin Basic/exercice/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 
     fun main() {
         println("ajouter un numbre")
-        val rows= 3;
+        val rows= readLine()!!.toInt();
         var i: Int=0
         var j: Int
         var space: Int
