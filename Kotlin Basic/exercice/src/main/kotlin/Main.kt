25f03a589042d662071645de5c8f9a3da05d0c4f
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-02-09 00:26:07 -0800

    exercice

---
+++ Kotlin Basic/exercice/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+
+    fun main() {
+
+        val rows=6
+        var i: Int=0
+        var j: Int
+        var space: Int
+        while (i<rows)
+        {
+            space=i
+            while (space<rows)
+            {
+                print(" ")
+                space++
+            }
+            j=0
+            while (j<i+1)
+            {
+                print("* ")
+                j++
+            }
+            println("")
+            i++
+        }
+
+    }
