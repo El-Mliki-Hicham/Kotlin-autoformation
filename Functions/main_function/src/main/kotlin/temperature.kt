42757f039e74b4833063d52117eecbd313e4fcb1
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-02-13 00:46:09 -0800

    organisation de code

--- Functions/main_function/src/main/kotlin/temperature.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    val temperature = 10
-    val isHot = if (temperature > 50) true else false
-    println(isHot)
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
-    println(message)
-
-}
