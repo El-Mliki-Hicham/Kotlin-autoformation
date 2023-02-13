feabd1d287961caa8a12509eac3f50860d67a65c
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-02-13 00:20:46 -0800

    constructor class

--- Classes and objects/Person/src/main/kotlin/Constructors/Person.kt
+++ Classes and objects/Person/src/main/kotlin/Constructors/Person.kt
@@ -6,6 +6,7 @@ class Person(name:String, age: Int , city : String) {
     var city :String=city
 
 
+
     fun AddPerson(){
      println("my name is $name am $age years old and i live in $city ")
 
